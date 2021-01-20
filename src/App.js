0438877b7aa319dd12d68f3fd8010e5f554f98ee
Jasmine Carter <heyjasminec@gmail.com>
2021-01-20 18:18:02 -0500

    Set up store, give App component access to dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { fetchProjects } from './actions/fetchProjects'
+
 
 class App extends React.Component {
 
+componentDidMount() {
+  this.props.fetchProjects()
+}
+
   render() {
     return (
-      <div>
+      <div className="App">
           APP
       </div>
     );
   }
 }
 
-export default App;
+
+export default connect(null, {fetchProjects})(App);
--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'; //allows us to create store, incoroporates thunk with store, allows to combine middlewares/chain them
 import thunk from 'redux-thunk'; //allows async requests
 import { Provider } from 'react-redux'; //Any component wrapped in Provider will have access to Redux store
+import projectReducer from './reducers/projectReducer'
+
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(projectReducer, composeEnhancers(applyMiddleware(thunk)));
 //reducer responsible for receiving actions, interpreting them, and adjusting store based on them
 
 
 
 ReactDOM.render(
   <Provider store={store} >
-  <App />,
-  </Provider>
+  <App />
+  </Provider>,
   document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
