7380637bf2dff544f39eb3f4a59d8146a08f63b4
Jasmine Carter <heyjasminec@gmail.com>
2021-01-19 20:28:21 -0500

    Set up store and enhancers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux'; //allows us to create store, incoroporates thunk with store
+import {createStore, applyMiddleware, compose} from 'redux'; //allows us to create store, incoroporates thunk with store, allows to combine middlewares/chain them
 import thunk from 'redux-thunk'; //allows async requests
 import { Provider } from 'react-redux'; //Any component wrapped in Provider will have access to Redux store
 
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-//set up store
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider >
+  <App />,
+  </Provider>
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
