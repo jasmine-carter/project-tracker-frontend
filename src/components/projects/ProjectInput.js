2fb300fa4869d6bd0472d718bfdec202d1480bb7
Jasmine Carter <heyjasminec@gmail.com>
2021-01-20 20:10:49 -0500

    Stub out basic project create form

--- src/components/projects/ProjectInput.js
+++ src/components/projects/ProjectInput.js
@@ -10,6 +10,26 @@ class ProjectsContainer extends Component {
     return (
       <div>
         ProjectInput
+        <form>
+          <label>Project Name:</label> {'  '}
+          <input
+          type="text"
+          name="project_name"
+        //  value={this.state.text}
+          placeholder="Project Name"/><br/>
+          <label>Project Description:</label>{'  '}
+          <input
+          type="text"
+          name="project_description"
+          placeholder="Project Description"
+          maxLength="200"
+          />
+          <br/>
+          <br/>
+          <input
+          type="submit"
+          value="Create Project"/>
+        </form>
       </div>
     )
   }
