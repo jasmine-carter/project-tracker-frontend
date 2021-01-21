f2d7331ba7646afe3e66ee47e61914011b26dcc8
Jasmine Carter <heyjasminec@gmail.com>
2021-01-20 19:53:11 -0500

    Set up ProjectList component to render projects from state

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import Project from './Project'
 //functional component responsible for rendering list of projects
 //will get projects from props from ProjectsContainer
 
 const ProjectList = (props) => {
-
+  console.log(props)
   return (
-    <div>
-      ProjectList
-    </div>
+    <ul>
+      {props.projects.map(project => <li key={project.id} >{project.name} - {project.description}</li> )}
+    </ul>
   )
-
 }
 
 export default ProjectList
