9a6c472695307d28414a4a233e17ad22da5c8deb
Jasmine Carter <heyjasminec@gmail.com>
2021-01-21 21:24:15 -0500

    Stub out Materials components

---
+++ src/components/materials/MaterialInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MaterialInput extends React.Component {
+
+  render() {
+    return (
+      <div>
+        MaterialInput
+      </div>
+    )
+  }
+}
+
+export default MaterialInput;
---
+++ src/components/materials/MaterialsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//functional component
+const MaterialsList = (props) => {
+
+  return (
+    <div>
+    </div>
+  )
+
+}
