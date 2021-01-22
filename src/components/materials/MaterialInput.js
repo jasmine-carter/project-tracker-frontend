328b57d9ea52dbfaa5c86996172e6ffee8c0839c
Jasmine Carter <heyjasminec@gmail.com>
2021-01-22 18:55:05 -0500

    Set up OnChange, handleSubmit and connect dispatch to form

--- src/components/materials/MaterialInput.js
+++ src/components/materials/MaterialInput.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {addMaterial} from '../../actions/addMaterial'
+import {connect} from 'react-redux'
 
 class MaterialInput extends React.Component {
 
@@ -8,40 +10,58 @@ class MaterialInput extends React.Component {
     quantity: ''
   }
 
+handleOnChange = (event) => {
+  this.setState({
+    [event.target.name]: event.target.value
+  })
+  console.log(this.state)
+}
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(this.state)
+  this.props.addMaterial(this.state, this.props.project.id)
+  this.setState({
+    name: '',
+    cost: '',
+    quantity: ''
+  })
+
+}
 
   render() {
     return (
       <div>
         Add Material to Your Project
-        <form>
-        <label>Material Name </label>{' '}
-        <input
-          type="text"
-          name="name"
-          value={this.state.name}
-          placeholder="Material Name"
-          onChange={this.handleOnChange}/><br/>
-        <label>Cost of Material</label>{' '}
-        <input
-          type="text"
-          name="cost"
-          value={this.state.cost}
-          placeholder="Material Cost"
-          onChange={this.handleOnChange}/><br/>
-        <label>Quantity Needed</label> {' '}
-        <input
-          type="text"
-          name="quantity"
-          value={this.state.quantity}
-          placeholder="Quantity Needed"
-          onChange={this.handleOnChange} /><br/>
-        <input
-          type="submit"
-          value="Create Project Material" />
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Material Name </label>{' '}
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            placeholder="Material Name"
+            onChange={this.handleOnChange}/><br/>
+          <label>Cost of Material</label>{' '}
+          <input
+            type="text"
+            name="cost"
+            value={this.state.cost}
+            placeholder="Material Cost"
+            onChange={this.handleOnChange}/><br/>
+          <label>Quantity Needed</label> {' '}
+          <input
+            type="text"
+            name="quantity"
+            value={this.state.quantity}
+            placeholder="Quantity Needed"
+            onChange={this.handleOnChange} /><br/>
+          <input
+            type="submit"
+            value="Create Project Material" />
         </form>
       </div>
     )
   }
 }
 
-export default MaterialInput;
+export default connect(null, {addMaterial})(MaterialInput);
