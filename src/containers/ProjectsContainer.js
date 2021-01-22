e74634c608191d9553b80f6694a3c63a5eca7c83
Jasmine Carter <heyjasminec@gmail.com>
2021-01-21 20:27:01 -0500

    Add Switch to render components base on route

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import ProjectList from '../components/projects/ProjectList'
 import ProjectInput from '../components/projects/ProjectInput'
 import {fetchProjects} from '../actions/fetchProjects'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Project from '../components/projects/Project'
 
 class ProjectsContainer extends Component {
@@ -15,9 +15,11 @@ componentDidMount(){
   render() {
     return(
       <div>
+      <Switch> //chooses the first route that matches the path
         <Route path='/projects/new' component={ProjectInput} />
         <Route path='/projects/:id' render={(routerProps) => <Project {...routerProps} projects={this.props.projects}/>}/>
         <Route exact path='/projects' render={(routerProps) => <ProjectList projects={this.props.projects}/>}/>
+      </Switch>
       </div>
     )
   }
