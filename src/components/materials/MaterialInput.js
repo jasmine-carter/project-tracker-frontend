a0745dad4f710805211d44251710d62e3bc9c7fa
Jasmine Carter <heyjasminec@gmail.com>
2021-01-22 09:35:52 -0500

    Build out material create form

--- src/components/materials/MaterialInput.js
+++ src/components/materials/MaterialInput.js
@@ -2,10 +2,43 @@ import React from 'react';
 
 class MaterialInput extends React.Component {
 
+  state = {
+    name: '',
+    cost: '',
+    quantity: ''
+  }
+
+
   render() {
     return (
       <div>
-        MaterialInput
+        Add Material to Your Project
+        <form>
+        <label>Material Name </label>{' '}
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          placeholder="Material Name"
+          onChange={this.handleOnChange}/><br/>
+        <label>Cost of Material</label>{' '}
+        <input
+          type="text"
+          name="cost"
+          value={this.state.cost}
+          placeholder="Material Cost"
+          onChange={this.handleOnChange}/><br/>
+        <label>Quantity Needed</label> {' '}
+        <input
+          type="text"
+          name="quantity"
+          value={this.state.quantity}
+          placeholder="Quantity Needed"
+          onChange={this.handleOnChange} /><br/>
+        <input
+          type="submit"
+          value="Create Project Material" />
+        </form>
       </div>
     )
   }
