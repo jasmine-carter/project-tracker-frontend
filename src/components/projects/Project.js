ddb05a297753af4dc9bd2a8c6679f0de7f087f04
Jasmine Carter <heyjasminec@gmail.com>
2021-01-21 19:50:37 -0500

    Allow component to have project set by props by parent, or by params in router

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 
-
 class Project extends Component {
 
 
   handleOnClick = () => {
     this.props.deleteProject(this.props.project.id)
   }
-render() {
-  const { project } = this.props;
 
+render() {
+  //let project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
+  //const { project } = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
+
+  let  { project } = {}
+  if (this.props.hasOwnProperty('match')) {
+    project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
+  }
+  else {
+    project = this.props
+  }
+
+
+  console.log(this.props.hasOwnProperty('match'))
   return (
     <div>
       <li >
-      {project.name} - {project.description}
-      <button> X </button>
+      {project ? project.name : null} - {project ? project.description : null} {' '}
+      <button onClick={this.handleOnClick}> X </button>
       </li>
     </div>
     )
@@ -22,3 +33,4 @@ render() {
 }
 
 export default Project;
+//{project ? project.name : null} - {project ? project.description : null} {' '}
