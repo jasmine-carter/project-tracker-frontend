6c269c343f5c1b4e3e4ca27bf9ccec507ea8dc1c
Jasmine Carter <heyjasminec@gmail.com>
2021-01-21 20:12:45 -0500

    Add links to individual projects

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Project from './Project'
+import {Route, Link} from 'react-router-dom'
+import {deleteProject} from '../../actions/deleteProject'
 //functional component responsible for rendering list of projects
 //will get projects from props from ProjectsContainer
 
 const ProjectList = (props) => {
-  console.log(props)
+
   return (
     <ul>
-      {props.projects.map(project => <li key={project.id} >{project.name} - {project.description}</li> )}
+      {props.projects.map(project =>
+        <li key={project.id}>
+          <Link to={`/projects/${project.id}`}>{project.name} - {project.description} </Link>
+        </li> )}
     </ul>
   )
 }
