08410609097fb1a5da89cf55cd157ca5baac867c
Jasmine Carter <heyjasminec@gmail.com>
2021-01-21 21:19:20 -0500

    Stub out MaterialsContainer

---
+++ src/containers/MaterialsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MaterialsContainer extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Materials Container!
+      </div>
+    )
+  }
+
+}
+
+
+export default MaterialsContainer;
