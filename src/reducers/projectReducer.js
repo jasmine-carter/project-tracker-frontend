05ffc776baff84afe73c2e3eff4fcdbb2ab5e9b8
Jasmine Carter <heyjasminec@gmail.com>
2021-01-22 18:43:42 -0500

    Set up ADD_PROJECT_MATERIAL action in reducer

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -17,9 +17,21 @@ export default function projectReducer(state = { projects: []}, action) {
     }
 
     case 'DELETE_PROJECT':
-    debugger;
-    const projects = state.projects.filter(project => project.id !== parseInt(action.payload))
+    ///debugger;
+    let projects = state.projects.filter(project => project.id !== parseInt(action.payload))
     return {...state, projects}
+
+    case 'ADD_PROJECT_MATERIAL':
+    debugger;
+      let projects1 = state.projects.map(project => {
+        if (project.id === action.payload.project_id) {
+          return action.payload
+        } else {
+          return project
+        }
+      })
+
+    return {...state, projects: projects1}
 
     default:
     return state;
