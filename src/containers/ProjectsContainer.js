236ee13a7b6b88cbee88c89b3d72062bd233256f
Jasmine Carter <heyjasminec@gmail.com>
2021-01-23 13:05:17 -0500

    Mount updateProject action creator and pass as props to Project component

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -4,6 +4,7 @@ import ProjectList from '../components/projects/ProjectList'
 import ProjectInput from '../components/projects/ProjectInput'
 import {fetchProjects} from '../actions/fetchProjects'
 import {deleteProject} from '../actions/deleteProject'
+import {updateProject} from '../actions/updateProject'
 import { Route, Switch } from 'react-router-dom'
 import Project from '../components/projects/Project'
 import NavBar from '../components/NavBar'
@@ -20,7 +21,7 @@ componentDidMount(){
       <NavBar/>
       <Switch> //chooses the first route that matches the path
         <Route path='/projects/new' component={ProjectInput} />
-        <Route path='/projects/:id' render={(routerProps) => <Project {...routerProps} projects={this.props.projects} deleteProject={this.props.deleteProject}/>}/>
+        <Route path='/projects/:id' render={(routerProps) => <Project {...routerProps} projects={this.props.projects} deleteProject={this.props.deleteProject} updateProject={this.props.updateProject}/>}/>
         <Route exact path='/projects' render={(routerProps) => <ProjectList projects={this.props.projects}/>}/>
       </Switch>
       </div>
@@ -34,9 +35,4 @@ const mapStateToProps = state => {
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-  addProject: text => dispatch({type: 'ADD_PROJECT', text}),
-  deleteProject: id => dispatch({type: 'DELETE_PROJECT', id})
-})
-
-export default connect(mapStateToProps, {fetchProjects, deleteProject})(ProjectsContainer);
+export default connect(mapStateToProps, {fetchProjects, deleteProject, updateProject})(ProjectsContainer);
