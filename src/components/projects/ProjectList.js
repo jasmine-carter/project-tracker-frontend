2d5dd070f46a9fe3ee9d827e96c47b8366b4ecf0
Jasmine Carter <heyjasminec@gmail.com>
2021-01-23 16:40:22 -0500

    Remove render logic to move to Project component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Project from './Project'
 import {Route, Link} from 'react-router-dom'
-import {deleteProject} from '../../actions/deleteProject'
 //functional component responsible for rendering list of projects
 //will get projects from props from ProjectsContainer
 
 const ProjectList = (props) => {
-
   return (
-    <ul>
-      {props.projects.map(project =>
-        <li key={project.id}>
-          <Link to={`/projects/${project.id}`}>{project.name} - {project.description} </Link>
-        </li> )}
-    </ul>
+    <div>
+      <h3>All Projects</h3>
+        <ul>
+          {props.projects.map(project =>
+            <li key={project.id}>
+              <Link to={`/projects/${project.id}`}>{project.name} </Link>
+            </li> )}
+        </ul>
+    </div>
   )
 }
 
