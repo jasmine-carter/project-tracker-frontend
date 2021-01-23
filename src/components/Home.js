712bd8494ad631f7257914d42a1869070a1b554d
Jasmine Carter <heyjasminec@gmail.com>
2021-01-23 15:55:52 -0500

    Finalize Home page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div class="Home">
+    <div>
+    <h3>Welcome!</h3>
+    <p>Keeping tabs on home improvement or art projects can sometimes be a pain when you're trying to stay in a budget.
+    Here you can create the project you want to keep tabs on, add materials and the quantities that you know you need to get for it, and track if you've purchased said materials.
+    All while seeing how much your project has cost!</p>
+    </div>
+      <img src="https://i2.wp.com/elocreations.com/wp-content/uploads/2020/07/IMG_7607-1.jpg?resize=768%2C1024&ssl=1"/>
+    </div>
+  )
+}
+
+export default Home;
