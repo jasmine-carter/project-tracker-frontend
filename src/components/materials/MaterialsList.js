fd1d46f68b74ef59d922197d99c1b07c612fbe40
Jasmine Carter <heyjasminec@gmail.com>
2021-01-23 20:09:36 -0500

    Styling for Materials List

--- src/components/materials/MaterialsList.js
+++ src/components/materials/MaterialsList.js
@@ -3,23 +3,23 @@ import Material from './Material'
 
 //functional component
 const MaterialsList = (props) => {
-  console.log(props)
 
   const {materials} = props
   const projectMaterials = materials.map(material => {
     return(
-      <div><Material
+      <div class="Material"><Material
       key={material.id}
       material = {material}
       />
       </div>
     )
   })
-
+  console.log(projectMaterials.length)
   return (
-    <div>
-    Materials Needed for this Project
-    {projectMaterials}
+    <div style={{background: "#ffdaae"}}>
+    <div class="MaterialsList">
+    {projectMaterials.length > 0 ? projectMaterials : "It looks like you need to add materials to your project!"}
+    </div>
     </div>
   )
 
