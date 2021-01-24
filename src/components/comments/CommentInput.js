25974a05a2aef07d69d22fda035dface7bae00ef
Jasmine Carter <heyjasminec@gmail.com>
2021-01-24 14:31:23 -0500

    configure form and form submit handling

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {addProjectComment} from '../../actions/addProjectComment'
 
 class CommentInput extends React.Component {
 
@@ -7,14 +8,37 @@ class CommentInput extends React.Component {
     content: ''
   }
 
-  render() {
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addProjectComment(this.state, this.props.project.id)
+  }
+
+  render() {
     return (
       <div>
+      <h4 style={{fontFamily: "Verdana"}}>Notes about this project</h4>
+      <form onSubmit={(event) =>this.handleSubmit(event)}>
+      <label>Note</label>
+      <input
+        type="text"
+        name="content"
+        value={this.state.content}
+        placeholder="Project Notes"
+        onChange={this.handleOnChange}/>
+      <input
+        type="submit"
+        value="Add Project Comment" />
+      </form>
       </div>
     )
   }
 
 }
 
-export default connect()(CommentInput);
+export default connect(null, {addProjectComment})(CommentInput);
