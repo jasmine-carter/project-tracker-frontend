d3484dc2445300aff44bb595b6c3970ba8b81f9c
Jasmine Carter <heyjasminec@gmail.com>
2021-01-24 14:07:56 -0500

    Set up action creator for adding project comment'

---
+++ src/actions/addProjectComment.js
@@ -0,0 +1,22 @@
+export function addProjectComment(data, project_id){
+
+  return (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/projects/${project_id}/comments`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(comment =>{
+      if (comment.error) {
+        alert(comment.error)
+      } else {
+        dispatch({type: 'ADD_PROJECT_COMMENT', payload: comment})
+      }
+    })
+  }
+
+}
