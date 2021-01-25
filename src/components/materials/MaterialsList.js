fbdd159796f41ca23b1d008a9a16d9c0b69e2014
Jasmine Carter <heyjasminec@gmail.com>
2021-01-25 16:42:42 -0500

    Refactor materialsList to handle when props === undefined or empty

--- src/components/materials/MaterialsList.js
+++ src/components/materials/MaterialsList.js
@@ -6,23 +6,35 @@ import Material from './Material'
 //functional component
 const MaterialsList = (props) => {
 
-  const {materials} = props
-  const projectMaterials = materials.map(material => {
-    return(
-      <div className="Material" key={material.id}><Material
-      key={material.id}
-      material = {material}
-      deleteMaterial={props.deleteMaterial}
-      updateMaterial={props.updateMaterial}
-      />
-      </div>
-    )
-  })
+function whatRenders(){
+  if (materials == undefined || materials.length === 0) {
+    return("There are no materials yet, add some below!")
+  } else {
+    const newMaterials = materials.map(material => {
+      return(
+        <div className="Material" key={material.id}><Material
+        key={material.id}
+        material = {material}
+        deleteMaterial={props.deleteMaterial}
+        updateMaterial={props.updateMaterial}
+        />
+        </div>
+      )
+    })
+     return newMaterials
 
+  }
+}
+
+
+  const {materials} = props
+
+  //debugger;
   return (
     <div style={{background: "#ffdaae"}}>
     <div class="MaterialsList">
-    {projectMaterials.length > 0 ? projectMaterials : "It looks like you need to add materials to your project!"}
+    {whatRenders()}
+    {console.log(whatRenders())}
     </div>
     </div>
   )
@@ -31,3 +43,16 @@ const MaterialsList = (props) => {
 
 export default MaterialsList;
 /*  <Material materials={props.materials}/>*/
+
+/*const projectMaterials = materials.map(material => {
+  return(
+    <div className="Material" key={material.id}><Material
+    key={material.id}
+    material = {material}
+    deleteMaterial={props.deleteMaterial}
+    updateMaterial={props.updateMaterial}
+    />
+    </div>
+  )
+})
+*/
