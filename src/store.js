b17243df566772b29063523f6f064675b5911a42
Jasmine Carter <heyjasminec@gmail.com>
2021-01-25 21:35:38 -0500

    pass rootReducer to createStore

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import projectReducer from './reducers/projectReducer.js';
+import commentReducer from './reducers/commentReducer';
 import thunk from 'redux-thunk';
-import projectReducer from './reducers/projectReducer.js'
-import commentReducer from './reducers/commentReducer'
 
 
-const reducer = combineReducers({
-  projects: projectReducer,
-  comments: commentReducer
+const rootReducer = combineReducers({
+    projects: projectReducer,
+    comments: commentReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(projectReducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 //reducer responsible for receiving actions, interpreting them, and adjusting store based on them
 
 export default store
