6d6fbade8c09f37f1cdc9d8860a1613f1e8da01f
Jasmine Carter <heyjasminec@gmail.com>
2021-01-26 11:01:28 -0500

    Evaluate what image to show upon render

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -16,35 +16,54 @@ class Project extends Component {
     let project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
     this.props.updateProject(project.id, project)
   }
+
+
 render() {
   //let project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
   //const { project } = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
-  //debugger
+
+  //if props.projects.length > 0 then project= props.projects.filter
   let project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
+  //debugger;
+      console.log(`Project mounted ${this.props}`)
+      console.log(this.props.match.params.id[0])
+
+  function imageRender() {
+    console.log(project.image == "")
+  	if (project != undefined) {
+  		if (project.image != "") {
+  			return (<img src={project.image}/>)
+  		}else{
+  			return(<img src="https://static.vecteezy.com/system/resources/previews/000/163/317/non_2x/doodled-blue-craft-tools-vector.jpg"/>)
+  		}
+  	}
+  }
+
 
   return (
     <div>
       <h1 id={project ? project.id : "none"}> {project ? project.name : "There's no Project here"} </h1>
       <div class="landscape">
-        <img src={project.image != "" ? project.image : "https://static.vecteezy.com/system/resources/previews/000/163/317/non_2x/doodled-blue-craft-tools-vector.jpg"}/>
+        {imageRender()}
       </div>
       <p>Project Details: {project ? project.description : null} {' '}<br/>
-      Project Cost: ${project.cost}<br/>
-      Project Completed: {project.completed.toString()} <button class="update-button" onClick={this.handleOnClick}>✓</button></p>
+      Project Cost: ${project != undefined ? project.cost : 0}<br/>
+      Project Completed: {project != undefined ? project.completed.toString() : "false"} <button class="update-button" onClick={this.handleOnClick}>✓</button></p>
       <button class="delete-button" onClick={this.handleOnDelete}> Delete Project </button>
       <br/>
       <br/>
       <h3 style={{fontFamily: "Verdana"}}>Materials Needed for this Project</h3>
       <MaterialsContainer project={project}/>
-      <CommentsContainer />
+      <CommentsContainer project={project}/>
     </div>
     )
   }
 }
 
 export default Project;
-///  <p>Project Cost: {project.cost} </p>
+//<img src={project != undefined ? project.image : "https://static.vecteezy.com/system/resources/previews/000/163/317/non_2x/doodled-blue-craft-tools-vector.jpg"}/>
 
+///<img src={project != undefined ? project.image : "https://static.vecteezy.com/system/resources/previews/000/163/317/non_2x/doodled-blue-craft-tools-vector.jpg"}/>
 /*let  project = {}
   if (this.props.hasOwnProperty('match')) {
     project = this.props.projects.filter(project => project.id == this.props.match.params.id)[0]
@@ -52,3 +71,4 @@ export default Project;
   else {
     project = this.props.project
   }*/
+//  <CommentsContainer project={project}/>
